1519add5136fc6f32063f82c841fef443e1353e8
worapong1423 <d4rkn3s4@email.com>
2019-10-14 01:39:54 +0700

    add hotel

--- src/store/hotel.js
+++ src/store/hotel.js
@@ -2,6 +2,7 @@ import axios from '../axios'
 
 import { make } from 'vuex-pathify'
 const state = {
+  hotel:[],
 
 }
 const getters = {
@@ -12,41 +13,58 @@ const mutations = make.mutations(state)
 
 const actions = {
 async getData() {
-let load = axios.get('/api/hotel')
+let load = await axios.get('/api/hotel')
   .then((r) => {
-
+    state.hotel = r.data
+    return true;
   }).catch((e) => {
-
+    alert(JSON.stringify(e.response));
+    return false;
 });
+    return load;
   },
 async storeData(context, params) {
-  let load = axios.post('/api/hotel', params)
+  let load = await axios.post('/api/hotel', params)
   .then((r) => {
     alert('Save Data Success');
+    return true;
   }).catch((e) => {
     alert('Error Save');
+    return false;
   });
+    return load;
 },
 
 async updateData(context, params) {
-  let load = axios.put('/api/hotel' + params.id, params)
+  let load = await axios.put(`/api/hotel/${params.id}`, params)
     .then((r) => {
     alert('Update Data Success');
+    return true;
   }).catch((e) => {
     alert('Error Update');
+    return false;
   });
+    return load
 },
 
 async destroyData(context, params) {
-  let load = axios.delete('/api/hotel' + params.id)
-    .then((r) => {
-    alert('Delete Data Success');
-  }).catch((e) => {
-    alert('Error Delete');
-  });
-  },
 
+  let confirms = confirm('Do you want to delete this data ?');
+  if(confirms){
+    let load = await axios.delete(`/api/test/${id}`)
+    .then(async (r) => {
+      alert('Delete Data Success');
+      await actions.read();
+    }).catch((e) => {
+      alert('Error Delete');
+    });
+    }
+
+  },
 }
+
+
+
 
 export default {
   namespaced: true,
