a7174f64d0f9057a51832e08a3508c8a680b4fc6
worapong1423 <d4rkn3s4@email.com>
2019-10-17 22:34:33 +0700

    .

--- src/components/page/hotel/listhotel.vue
+++ src/components/page/hotel/listhotel.vue
@@ -8,7 +8,7 @@
         <div v-for="hotel,index in hotel" :key="index">
         <q-list bordered separator>
             <q-item clickable v-ripple >
-              <q-item-section @click="openpage()">{{(index+1)}}.  {{hotel.Hotel_name}}</q-item-section>
+              <q-item-section @click="openpage(hotel.id)">{{(index+1)}}.  {{hotel.Hotel_name}}</q-item-section>
                <q-item-section top side>
           <div class="text-grey-8 q-gutter-xs">
 
@@ -66,8 +66,8 @@ methods:{
     /******* Methods default run ******/
 
 
-    async openpage() {
-          this.$router.push('/hotel/layouttab/tablist')
+    async openpage(id) {
+          this.$router.push(`/hotel/layouttab/tablist?id=${id}`)
 
 
         },
