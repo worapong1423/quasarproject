bca0d25a3df3874c32d7bcbc559b030ac721bbac
worapong1423 <d4rkn3s4@email.com>
2019-10-21 22:45:37 +0700

    Merge branch 'master' of https://github.com/worapong1423/quasarproject
    # Conflicts:
    #	src/components/page/hotel/tabhotel/rate.vue

--- src/components/page/hotel/tabhotel/rate.vue
+++ src/components/page/hotel/tabhotel/rate.vue
@@ -1,21 +1,68 @@
 <!----------Make By YourName---------------->
 <template>
+  <div style="padding:5%;">
+    <div v-if="!typerate">
+      <q-markup-table>
+        <thead>
+        <tr>
+          <th class="text-left">รายการ</th>
+          <th class="text-right">บาท/ชิ้น</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td class="text-left">{{hotel.Hotel_name}}</td>
+          <td class="text-right">{{hotel.Hotel_name}}</td>
+        </tr>
+        </tbody>
+      </q-markup-table>
 
+      <q-page-sticky position="bottom-right" :offset="[18, 18]">
+        <q-btn v-on:click="typerate=false" fab icon="edit" color="primary"  />
+      </q-page-sticky>
+
+    </div>
+
+    <div v-else-if="typerate">
+      <q-markup-table>
+        <thead>
+        <tr>
+          <th class="text-left">รายการ</th>
+          <th class="text-right">บาท/ชิ้น</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td class="text-left">{{hotel.Hotel_name}}</td>
+          <td class="text-right">
+            <q-input outlined v-model=""/>
+          </td>
+        </tr>
+        </tbody>
+      </q-markup-table>
+    </div>
+
+
+
+  </div>
 </template>
 
 <script>
-    import {get, sync, call} from "vuex-pathify";
-
+    import { get,sync,call } from "vuex-pathify";
     export default {
         name: 'Root',
         /*-------------------------Load Component---------------------------------------*/
-        components: {},
+        components: {
+
+        },
         /*-------------------------Set Component---------------------------------------*/
-        props: {},
+        props:{
+
+        },
         /*-------------------------DataVarible---------------------------------------*/
         data() {
             return {
-                ilke: false,
+                typerate: false
             };
         },
         /*-------------------------Run Methods when Start this Page------------------------------------------*/
@@ -28,16 +75,14 @@
             next()
         },
         /*-------------------------Vuex Methods and Couputed Methods------------------------------------------*/
-        computed: {
-            ...call('rate/*')
+        computed:{
 
         },
         /*-------------------------Methods------------------------------------------*/
-        methods: {
-            ...sync('rate/*'),
+        methods:{
             /******* Methods default run ******/
-            load: async function () {
-                let id = this.$route.params.id;
+            load:async function(){
+                let id =this.$route.params.id;
                 await this.readOne(id);
             }
         },
