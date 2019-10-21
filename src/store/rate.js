ff5a2c94b6f6aea8b1afe270d97d79670d345f9c
worapong1423 <d4rkn3s4@email.com>
2019-10-21 22:38:27 +0700

    .

--- src/store/rate.js
+++ src/store/rate.js
@@ -2,7 +2,7 @@ import axios from '../axios'
 
 import { make } from 'vuex-pathify'
 const state = {
-
+  rateData : {},
 }
 const getters = {
 
@@ -11,8 +11,18 @@ const getters = {
 const mutations = make.mutations(state)
 
 const actions = {
+  async readOne(context,id){
+    let x = await axios.get(`/api/rate/${id}`)
+      .then(async (r) => {
+        state.hotelData = r.data
+        console.log(r.data);
+      }).catch((e) => {
+
+      });
+  },
 
 async NameMethods(context,params){
+
 
 }
 
